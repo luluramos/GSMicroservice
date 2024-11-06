fb85e723decf561f89ec530c596c5c40c0f04f2a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-06 20:49:14 -0300

    kk

--- worker/worker.js
+++ worker/worker.js
@@ -21,7 +21,11 @@ function replaceVariables(template, data) {
 
 // Função para gerar o PDF a partir de HTML
 async function generatePDF(htmlContent, outputPath) {
-  const browser = await puppeteer.launch();
+  // Adicionando as opções --no-sandbox e --disable-setuid-sandbox
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
   await page.pdf({ path: outputPath, format: 'A4', landscape: true });
